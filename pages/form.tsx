bc52792a1a10b10d6b71312eb9773321e72b9768
junezhuo <yzzhuo2@gmail.com>
2024-04-09 19:02:42 +0300

    feat: update the index page to explore page

---
+++ pages/form.tsx
@@ -0,0 +1,192 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useRef, useState } from 'react';
+import Header from '../components/Header';
+import { useCompletion } from 'ai/react';
+import LoadingDots from '../components/LoadingDots';
+import { MapIcon } from '@heroicons/react/24/outline';
+import TravelPlanReport, { PlanResult, TravelPreference, generatePromptForTravelPlan } from '../components/TravelPlanReport';
+
+
+const Home: NextPage = () => {
+  const [preferences, setPreferences] = useState<TravelPreference>({
+    destination: 'Helsinki',
+    startDate: new Date().toISOString().split('T')[0],
+    duration: 1,
+  });
+  const [loading, setLoading] = useState(false);
+  const { complete } = useCompletion({
+    api: '/api/completion',
+  });
+  const [data, setData] = useState<PlanResult>({
+    startDate: '',
+    duration: '',
+    city: '',
+    country: '',
+    itinerary: [],
+  });
+  const [finished, setFinished] = useState(false);
+
+  const generatePlan = async (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log({ preferences });
+    const prompt = generatePromptForTravelPlan(preferences);
+    console.log({ prompt });
+    const maxRetries = 3;
+    for (let i = 0; i < maxRetries; i++) {
+      try {
+        const response = await complete(prompt);
+        console.log({ response });
+        const result = JSON.parse(response as string);
+        console.log({ result });
+        setData(result);
+        setFinished(true);
+        break; // If successful, break the loop
+      } catch (error) {
+        console.error(error);
+        // If it's the last iteration, rethrow the error
+        if (i === maxRetries - 1) throw error;
+      }
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Head>
+        <title>Place explore</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <main className="w-screen h-screen">
+        {!finished ? <form className="flex flex-col items-center justify-center w-full ">
+          <h1 className="text-3xl font-bold my-4 pt-4">Where do you want to go?</h1>
+          <div className="max-w-xl w-full">
+          <div className="flex mt-10 items-center space-x-3">
+            <p className="text-left font-bold">
+              Desination
+              <span className="text-slate-500">(City or Country)</span>
+            </p>
+          </div>
+          <input
+            value={preferences?.destination}
+            onChange={(e) => 
+              setPreferences({
+                ...preferences,
+                destination: e.target.value,
+              })
+            }
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            placeholder={'e.g. Helsinki'}
+          />
+          <div className='w-full grid grid-cols-2 gap-2'>
+            <div className="col-span-1">
+              <p className="text-left font-bold">
+                Start date
+              </p>
+              <input
+                type="date"
+                placeholder="Start date"
+                className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+                value={preferences?.startDate}
+                onChange={(e) => 
+                  setPreferences({
+                    ...preferences,
+                    startDate: e.target.value,
+                  })
+                }
+              />
+            </div>
+            <div className="col-span-1">
+              <p className="text-left font-bold">
+                Duration
+                <span className="text-slate-500"> (Days)</span>
+              </p>
+              <input
+                type="number"
+                placeholder="days"
+                className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+                value={preferences?.duration}
+                onChange={(e) => 
+                  setPreferences({
+                    ...preferences,
+                    duration: parseInt(e.target.value),
+                  })
+                }
+              />
+            </div>
+          </div>
+          <div className='w-full flex flex-col'>
+              <p className="text-left font-bold">
+                Interests
+              <span className="text-slate-500"> (Optional)</span>
+              </p>
+              <textarea
+                value={preferences?.places}
+                onChange={(e) => 
+                  setPreferences({
+                    ...preferences,
+                    interests: e.target.value,
+                  })
+                }
+                rows={3}
+                className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+                placeholder={'you could tell places you want to visit or activities you want to do'}
+              />
+          </div>
+          <div className='w-full flex flex-col'>
+              <p className="text-left font-bold">
+                Dietary preference
+                <span className="text-slate-500"> (Optional)</span>
+              </p>
+              <textarea
+                value={preferences?.dietary}
+                onChange={(e) => 
+                  setPreferences({
+                    ...preferences,
+                    dietary: e.target.value,
+                  })
+                }
+                rows={2}
+                className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+                placeholder={'e.g. I am a vegetarian and I want to try local vegetarian food'}
+              />
+          </div>
+          {loading ? <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              disabled
+            >
+              <LoadingDots color="white" style="large" />
+            </button>: <button
+            className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+            onClick={generatePlan}
+            >
+             Generate
+          </button>
+        }
+        </div>
+        </form> :
+        <div className='flex flex-col justify-center items-center my-4'>
+          <div className='flex justify-end max-w-5xl w-full'>
+            <button
+             className='btn btn-sm text-white btn-neutral'
+             onClick={generatePlan}
+            >{
+              loading ? <LoadingDots color="white" style="small" /> : 'Regenerate'
+            }</button>
+          </div>
+          <TravelPlanReport 
+            data={data} 
+            preferences={preferences}
+            onChangeData={setData}
+          />
+        </div>
+        }
+      </main>
+    </div>
+  );
+};
+
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,178 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { useRef, useState } from 'react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import { LoadingCircle, SendIcon } from '../components/icons';
+import {ChevronRightIcon, ChevronDownIcon} from '@heroicons/react/24/outline'
+import Markdown from 'react-markdown'
 import { useCompletion } from 'ai/react';
-import LoadingDots from '../components/LoadingDots';
-import { MapIcon } from '@heroicons/react/24/outline';
 import TravelPlanReport, { PlanResult, TravelPreference, generatePromptForTravelPlan } from '../components/TravelPlanReport';
+import LoadingDots from '../components/LoadingDots';
 
+const dialogues = [
+ {
+    question: 'What is your destination?',
+    id: 'destination',
+    tips: [{
+      text: 'Not sure which destination to go to? Need some recommendations?',
+      prompt: (preference: TravelPreference) => 'please recommend some cities of Europe with brief reason for my recent travel?(city be bolded)'
+    }]
+ },
+ {
+    question: 'When do you plan to travel?',
+    id: 'travelDate',
+    tips:[{
+      text: 'Want to know the best time to travel to your destination?',
+      prompt: (preference: TravelPreference) =>`what is the best time to visit ${preference.destination}?`
+    }, {
+      text: 'Want a weather overview of your destination?',
+      prompt: (preference: TravelPreference) => `Give me the weather overview of ${preference.destination}?`
+    }]
+ },
+ {
+    question: 'How long will your trip be?',
+    id: 'tripLength',
+    tips: [{
+      text: 'Suggested duration to visit the destination?',
+      prompt: (preference: TravelPreference) => `what is the suggested duration to visit ${preference.destination}?`
+    }]
+ },
+ {
+  question: 'Do you have any interested specific landmarks or activities?',
+  id: 'interests',
+  tips:[{
+    text: 'What are the main local attractions, list the must-try local specialties?',
+    prompt: (preference: TravelPreference) => `What are the main local attractions in ${preference.destination}?`
+  }, {
+      text: 'Want to know the main local travelling activities?',
+      prompt: (preference: TravelPreference) => `What are the main local activities in ${preference.destination}, give me some options I could select with bold text?`
+    }
+  ]
+ },
+ {
+    question: 'Do you have any dietary restrictions or do you have local food you want to try?',
+    id: 'dietary',
+    tips: [
+      {
+        text: 'What are the local food specialties?',
+        prompt: (preference: TravelPreference) => `What are the local food specialties in ${preference.destination}?`
+      },
+      {
+        text: 'Local restaurant price ranges and options?',
+        prompt: (preference: TravelPreference) => `What are the local restaurant price ranges and options in ${preference.destination}?`
+      }
+    ]
+ },
+//  {
+//   id: 'other',
+//   question: 'Any other preferences or tips you would like to know?',
+//   tips: [{
+//     text: 'currency exchange rate?',
+//     prompt: (preference: TravelPreference) => `What is the currency exchange rate in ${preference.destination}?`
+//   }, {
+//     text: 'local language?',
+//     prompt: (preference: TravelPreference) => `What are the local language in ${preference.destination}?`
+//   }]
+// }
+]
+
+interface UITip {
+  value: string;
+  open: boolean;
+}
 
 const Home: NextPage = () => {
-  const [preferences, setPreferences] = useState<TravelPreference>({
-    destination: 'Helsinki',
-    startDate: new Date().toISOString().split('T')[0],
-    duration: 1,
-  });
-  const [loading, setLoading] = useState(false);
-  const { complete } = useCompletion({
+  const { completion, complete } =
+  useCompletion({
     api: '/api/completion',
   });
-  const [data, setData] = useState<PlanResult>({
+  const [loading, setLoading] = useState(false);
+  const [step, setStep] = useState(0);
+  const [inputText, setInputText] = useState('');
+  const [preference, setPreference] = useState<TravelPreference>({
+    destination: '',
+    startDate: '',
+    duration: 1,
+    places: '',
+    activities: '',
+    dietary: '',
+  });
+  const [data, setData] = useState<PlanResult>(
+    {
     startDate: '',
     duration: '',
     city: '',
     country: '',
     itinerary: [],
-  });
+  }
+  );
+  const [tipResult, setTipResult] = useState<UITip[]>([])
+  const [finishedInput, setFinishedInput] = useState(false);
   const [finished, setFinished] = useState(false);
 
-  const generatePlan = async (e: any) => {
-    e.preventDefault();
+  useEffect(() => {
+    const currentTips = dialogues[step].tips.map((tip) => {
+      return {
+        value: '',
+        open: false
+      }
+    });
+    setTipResult(currentTips)
+  },
+  [step])
+
+useEffect(() => {
+    if (completion) {
+      if (finishedInput) {
+        return;
+      } else {
+        const updatedTipResult = tipResult.map((tip, index) => {
+          if (tip.open) {
+            return {
+              ...tip,
+              value: completion
+            }
+          }
+          return tip;
+        })
+        setTipResult(updatedTipResult);
+      }
+    }
+  }, [completion])
+
+  const handleNext = (e: any) => {
+    if (!inputText) return;
+    const updatedPreference = {
+        ...preference,
+        [dialogues[step].id]: inputText,
+    }
+    setPreference(updatedPreference);
+    console.log({ preference });
+    setInputText('');
+    if (step === dialogues.length - 1) {
+      generatePlan();
+      setFinishedInput(true);
+    } else {
+      setStep(step + 1);
+    }
+  };
+
+  const generateAnswer = async (tipIndex: number) => {
     setLoading(true);
-    console.log({ preferences });
-    const prompt = generatePromptForTravelPlan(preferences);
+    const prompt = `${dialogues[step].tips[tipIndex].prompt(preference)}`;
+    console.log({ prompt });
+    const response = await complete(`${prompt}, anwser the question directly with content that are easy to read and clearly structured markdown format(for example use title/bold/list smartly).`);
+    console.log({ response });
+    setLoading(false);
+  };
+
+  const generatePlan = async () => {
+    setLoading(true);
+    const prompt = generatePromptForTravelPlan(preference);
+    console.log({ prompt });
+    const response = await complete(prompt);
+    console.log({ response });
     console.log({ prompt });
     const maxRetries = 3;
     for (let i = 0; i < maxRetries; i++) {
@@ -50,140 +191,144 @@ const Home: NextPage = () => {
       }
     }
     setLoading(false);
-  };
+  }
 
+  const handlClickTip = (index: number) => {
+    const isOpen = tipResult[index].open;
+    // if (!isOpen && !tipResult[index].value) {
+      if (!isOpen) {
+      generateAnswer(index);
+    }
+    const newTipResult = tipResult.map((tip, i) => {
+      if (i === index) {
+        return {
+          ...tip,
+          open: !tip.open
+        }
+      }
+      return {
+        ...tip,
+        open: false
+      };
+    })
+    setTipResult(newTipResult);
+  }
+
+  const renderStep = () => {
+      return <>
+        <div className="flex-col justify-start items-start space-x-3">
+          <p className="text-left font-medium flex-auto">
+            <span
+              className="font-semibold text-white w-6 h-6 inline-flex justify-center rounded-full bg-black mr-1"
+            >{step + 1}</span>
+            {dialogues[step].question}
+            {/* <span className="text-slate-500">(city)</span> */}
+          </p>
+          <div className='flex flex-col mt-4 gap-2'>
+          {
+            dialogues[step].tips && dialogues[step].tips.length > 0 ? 
+              dialogues[step].tips.map((tip, index) => {
+                return  <button
+                className="rounded-md border border-gray-200 bg-white text-left text-sm text-gray-500 transition-all duration-75 hover:gray-800 active:bg-gray-50"
+                onClick={(e) => {
+                  handlClickTip(index)
+                }}
+              >
+                <div className='flex gap-2 items-center bg-blue-100	'>
+                  <span className='font-bold text-blue-800 px-4 py-2'>{tip.text}</span>
+                  { tipResult[index] && tipResult[index].open ?
+                  <ChevronDownIcon className="h-4 w-4 text-blue-800" /> : <ChevronRightIcon className="h-4 w-4 text-blue-800" />}
+                </div>
+                { tipResult[index] && tipResult[index].open && <Markdown className='text-sm py-4 p-4'>
+                  {tipResult[index].value ? tipResult[index].value : 'loading...'}
+                </Markdown> }
+              </button>
+              })
+             : null
+          }
+          </div>
+        </div>
+      </>
+  }
+
+  const renderPlan = () => {
+    return <div className='text-left'>
+      {/* <Markdown remarkPlugins={[remarkGfm]}>{planResult}</Markdown> */}
+      <TravelPlanReport 
+        data={data} 
+        preferences={preference}
+        onChangeData={setData}
+        />
+    </div>
+  }
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Place explore</title>
+        <title>Travel Plan Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      <main className="w-screen h-screen">
-        {!finished ? <form className="flex flex-col items-center justify-center w-full ">
-          <h1 className="text-3xl font-bold my-4 pt-4">Where do you want to go?</h1>
-          <div className="max-w-xl w-full">
-          <div className="flex mt-10 items-center space-x-3">
-            <p className="text-left font-bold">
-              Desination
-              <span className="text-slate-500">(City or Country)</span>
+      <main className="flex flex-1 w-full flex-col items-center  text-center px-4 ">
+      {finished ? renderPlan() :<> <div className="border-gray-200sm:mx-0 mx-5 mt-5 max-w-screen-md rounded-md border sm:w-full">
+          <div className="flex flex-col space-y-4 p-7 sm:p-10 items-center bg-white shadow-lg">
+            <Image
+              src="/avatar.png"
+              alt="tourbuddy"
+              width={40}
+              height={40}
+              className="h-18 w-20"
+            />
+            <h1 className="text-lg font-semibold text-black">
+              Hi, I'm Tourbuddy!
+            </h1>
+            <p className="text-gray-500">
+              I'm an AI bot help you to make your personalized travel plan
             </p>
           </div>
+          <div className='w-full relative h-1 bg-gray-50'>
+            <div className={`absolute h-1 bg-orange-500`} style={{
+              width: `${(step + 1) * 100 / dialogues.length}%`
+            }}></div>
+          </div>
+          <div className="flex flex-col space-y-4 border-t border-gray-200 bg-gray-50 p-4 sm:p-10">
+         
+             {  finishedInput ? 
+             <div className='flex flex-col justify-center items-center gap-2'>
+              <LoadingDots color="black" style="large" />
+               Generating, Please wait a moment
+             </div>
+            :
+             renderStep()}
+          </div>
+        </div>
+        <div className="fixed bottom-0 flex w-full flex-col items-center space-y-3 bg-gradient-to-b from-transparent via-gray-100 to-gray-100 p-5 pb-3 sm:px-0">
+        {!finishedInput ? <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleNext(e);
+          }}
+          className="relative w-full max-w-screen-md rounded-xl border border-gray-200 bg-white px-4 pb-2 pt-3 shadow-lg sm:pb-3 sm:pt-4"
+        >
           <input
-            value={preferences?.destination}
-            onChange={(e) => 
-              setPreferences({
-                ...preferences,
-                destination: e.target.value,
-              })
-            }
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-            placeholder={'e.g. Helsinki'}
-          />
-          <div className='w-full grid grid-cols-2 gap-2'>
-            <div className="col-span-1">
-              <p className="text-left font-bold">
-                Start date
-              </p>
-              <input
-                type="date"
-                placeholder="Start date"
-                className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-                value={preferences?.startDate}
-                onChange={(e) => 
-                  setPreferences({
-                    ...preferences,
-                    startDate: e.target.value,
-                  })
-                }
-              />
-            </div>
-            <div className="col-span-1">
-              <p className="text-left font-bold">
-                Duration
-                <span className="text-slate-500"> (Days)</span>
-              </p>
-              <input
-                type="number"
-                placeholder="days"
-                className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-                value={preferences?.duration}
-                onChange={(e) => 
-                  setPreferences({
-                    ...preferences,
-                    duration: parseInt(e.target.value),
-                  })
-                }
-              />
-            </div>
-          </div>
-          <div className='w-full flex flex-col'>
-              <p className="text-left font-bold">
-                Interests
-              <span className="text-slate-500"> (Optional)</span>
-              </p>
-              <textarea
-                value={preferences?.places}
-                onChange={(e) => 
-                  setPreferences({
-                    ...preferences,
-                    interests: e.target.value,
-                  })
-                }
-                rows={3}
-                className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-                placeholder={'you could tell places you want to visit or activities you want to do'}
-              />
-          </div>
-          <div className='w-full flex flex-col'>
-              <p className="text-left font-bold">
-                Dietary preference
-                <span className="text-slate-500"> (Optional)</span>
-              </p>
-              <textarea
-                value={preferences?.dietary}
-                onChange={(e) => 
-                  setPreferences({
-                    ...preferences,
-                    dietary: e.target.value,
-                  })
-                }
-                rows={2}
-                className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-                placeholder={'e.g. I am a vegetarian and I want to try local vegetarian food'}
-              />
-          </div>
-          {loading ? <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              disabled
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black"
+            />
+            <button className='absolute inset-y-0 right-3 top-1 my-auto flex h-8 w-8 items-center justify-center rounded-md transition-all'
             >
-              <LoadingDots color="white" style="large" />
-            </button>: <button
-            className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-            onClick={generatePlan}
-            >
-             Generate
-          </button>
-        }
-        </div>
-        </form> :
-        <div className='flex flex-col justify-center items-center my-4'>
-          <div className='flex justify-end max-w-5xl w-full'>
-            <button
-             className='btn btn-sm text-white btn-neutral'
-             onClick={generatePlan}
-            >{
-              loading ? <LoadingDots color="white" style="small" /> : 'Regenerate'
-            }</button>
-          </div>
-          <TravelPlanReport 
-            data={data} 
-            preferences={preferences}
-            onChangeData={setData}
-          />
-        </div>
-        }
+              {loading ? (
+                  <LoadingCircle />
+                ) : (
+                  <SendIcon
+                    className="h-4 w-4 text-gray-300"
+                  />
+                )}
+            </button>
+        </form>  : null}
+      </div>
+      </>
+      } 
       </main>
     </div>
   );
