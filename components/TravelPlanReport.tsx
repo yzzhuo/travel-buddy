e1e7c6da5b1580be76bb89f5e95f131615d0a813
junezhuo <yzzhuo2@gmail.com>
2024-04-10 16:40:37 +0300

    fix: avoid background block the text

--- components/TravelPlanReport.tsx
+++ components/TravelPlanReport.tsx
@@ -113,7 +113,7 @@ export default function TravelPlanReport({preferences, data, onChangeData}:
           <div className="prose prose-sm md:prose md:block hidden">
             <h2>Itinerary Summary</h2>
           </div>
-          <div className="overflow-x-auto mt-4">
+          <div className="overflow-x-auto mt-4 bg-white shadow-xl">
             <table className="table table-fixed border hidden md:table">
               <thead className='bg-slate-400 text-white'>
                 <tr>
@@ -149,92 +149,90 @@ export default function TravelPlanReport({preferences, data, onChangeData}:
       {
         data.itinerary.map((item: any, index: number) => (
           <div key={index} className="col-span-12">
-            <section className=''>
-              <div >
-                <div className="prose">
-                  <h3>{item.date}</h3>
-                  <div className='flex md:flex-row gap-4 flex-col'>
-                    <span>City: {item.city}</span>
-                    <div className='flex gap-2'>Transportation: 
-                    <select className="select select-sm text-sm py-1" 
-                      value={item.transportation}
-                      onChange={(e) => {
-                        console.log(e.target.value);
-                        const newItinerary = data.itinerary.map((it: any, i: number) => {
-                          if (i === index) {
-                            it.transportation = e.target.value;
-                          }
-                          return it;
-                        });
-                        onChangeData({...data, itinerary: newItinerary});
-                      }}
-                    >
-                        {['driving', 'walking', 'bicycling'].map((transportation, index) => (
-                          <option 
-                          key={index} 
-                          value={transportation} 
-                          selected={item.transportation === transportation}
-                          >
-                          {transportation}
-                          </option>
-                        ))
+            <section className='bg-white px-4 py-2 rounded-lg shadow-lg'>
+              <div className="prose">
+                <h3>{item.date}</h3>
+                <div className='flex md:flex-row gap-4 flex-col'>
+                  <span>City: {item.city}</span>
+                  <div className='flex gap-2'>Transportation: 
+                  <select className="select select-sm text-sm py-1" 
+                    value={item.transportation}
+                    onChange={(e) => {
+                      console.log(e.target.value);
+                      const newItinerary = data.itinerary.map((it: any, i: number) => {
+                        if (i === index) {
+                          it.transportation = e.target.value;
                         }
-                      </select>
-                      </div>
-                  </div>
-                </div>
-                <div className="grid mt-4  flex-col md:grid-cols-12 w-full gap-4">
-                  <div className="col-span-6">
-                  <iframe
-                      frameBorder="0"
-                      height={750}
-                      className='h-56 md:h-96 shadow-xl'
-                      style={{border:0, width: '100%', maxWidth: '100%'}}
-                      referrerPolicy="no-referrer-when-downgrade"
-                      src={`https://www.google.com/maps/embed/v1/directions?key=${apiKey}&${generateDirectionQuery(item.places_to_visit)}&mode=${getTransportationMode(item.transportation)}`}
-                      allowFullScreen
-                    />
-                  </div>
-                  <div className='col-span-6 bg-white p-2 rounded-lg shadow-lg'>
-                    <div className='prose'>
-                      <h4 className='font-bold'>Route</h4>
-                      <p>{item.places_to_visit.map((place: any, index: number) => (
-                        <span key={index}>{place.name} 
-                          {index === item.places_to_visit.length - 1 ? '' : ' → '}
-                        </span>
-                      ))}</p>
+                        return it;
+                      });
+                      onChangeData({...data, itinerary: newItinerary});
+                    }}
+                  >
+                      {['driving', 'walking', 'bicycling'].map((transportation, index) => (
+                        <option 
+                        key={index} 
+                        value={transportation} 
+                        selected={item.transportation === transportation}
+                        >
+                        {transportation}
+                        </option>
+                      ))
+                      }
+                    </select>
                     </div>
-                    <div className="prose mt-4">
-                    <h4>Places</h4>
-                    <ul className='list-none ml-0 pl-0'>
-                      {item.places_to_visit.map((place: any, index: number) => (
-                        <li key={index}>
-                          <h5 className='font-bold'> 
-                            <span className='bg-primary text-white rounded-full w-6 h-6 inline-flex mr-2 justify-center items-center'>
-                              {index + 1}
-                            </span>
-                            {place.name}
-                            <a href={`https://www.google.com/maps/search/?api=1&query=${item.city},${place.name}`} target="_blank" rel="noreferrer" className="text-primary ml-2">
-                              <MapIcon className="w-4 h-4 inline-block text-geay" />
-                            </a>
-                          </h5>
-                          {/* <div className='flex gap-4'>
-                            <span>Address: {place.address}</span>
-                            <span>Transportation: {place.transportation}</span>
-                            <span>Price: {place.price}</span>
-                          </div> */}
-                          <p>{place.brief_intro}</p>
-                          {place.tip && <div role="alert" className="alert px-2 py-1 flex justify-start">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                            <span className='text-left text-xs md:text-sm flex-auto'>Tip: {place.tip}</span>
-                          </div>
-                          }
-                        </li>
-                      ))}
-                    </ul>
+                </div>
+              </div>
+              <div className="grid mt-4  flex-col md:grid-cols-12 w-full gap-4">
+                <div className="col-span-6">
+                <iframe
+                    frameBorder="0"
+                    height={750}
+                    className='h-56 md:h-96 shadow-xl'
+                    style={{border:0, width: '100%', maxWidth: '100%'}}
+                    referrerPolicy="no-referrer-when-downgrade"
+                    src={`https://www.google.com/maps/embed/v1/directions?key=${apiKey}&${generateDirectionQuery(item.places_to_visit)}&mode=${getTransportationMode(item.transportation)}`}
+                    allowFullScreen
+                  />
+                </div>
+                <div className='col-span-6'>
+                  <div className='prose'>
+                    <h4 className='font-bold'>Route</h4>
+                    <p>{item.places_to_visit.map((place: any, index: number) => (
+                      <span key={index}>{place.name} 
+                        {index === item.places_to_visit.length - 1 ? '' : ' → '}
+                      </span>
+                    ))}</p>
                   </div>
+                  <div className="prose mt-4">
+                  <h4>Places</h4>
+                  <ul className='list-none ml-0 pl-0'>
+                    {item.places_to_visit.map((place: any, index: number) => (
+                      <li key={index}>
+                        <h5 className='font-bold'> 
+                          <span className='bg-primary text-white rounded-full w-6 h-6 inline-flex mr-2 justify-center items-center'>
+                            {index + 1}
+                          </span>
+                          {place.name}
+                          <a href={`https://www.google.com/maps/search/?api=1&query=${item.city},${place.name}`} target="_blank" rel="noreferrer" className="text-primary ml-2">
+                            <MapIcon className="w-4 h-4 inline-block text-geay" />
+                          </a>
+                        </h5>
+                        {/* <div className='flex gap-4'>
+                          <span>Address: {place.address}</span>
+                          <span>Transportation: {place.transportation}</span>
+                          <span>Price: {place.price}</span>
+                        </div> */}
+                        <p>{place.brief_intro}</p>
+                        {place.tip && <div role="alert" className="alert px-2 py-1 flex justify-start">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                          <span className='text-left text-xs md:text-sm flex-auto'>Tip: {place.tip}</span>
+                        </div>
+                        }
+                      </li>
+                    ))}
+                  </ul>
                 </div>
-                </div>
+              </div>
               </div>
             </section>
           </div>
